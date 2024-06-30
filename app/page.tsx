c90bca08b856769721d188d275c317dc67156714
Rohan_Islam <rohanislam3669@gmail.com>
2024-07-01 01:37:52 +0600

    up

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <main className="bg-gray-950"></main>;
+  return <main className=""></main>;
 }
--- components/home-section/navbar.tsx
+++ components/home-section/navbar.tsx
@@ -1,12 +1,20 @@
-/* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
 import React from "react";
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className="container">
+    <nav className="bg-black ">
+      <div className="container flex flex-nowrap  justify-between items-center">
         <div>
-          <img src="/image.png" alt="" />
+          <Image width={89} height={77} src="/image.png" alt="" />
+        </div>
+        <div className="text-white">
+          <link rel="stylesheet" href="" />
+
+          <p className="relative after:absolute hover:bg-blue-600 hover:h-[2px] hover:w-[100%] cursor-pointer">
+            Home
+          </p>
+          <link />
         </div>
       </div>
     </nav>
