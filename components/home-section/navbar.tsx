5b7f4a9d6ae4783e38cfc09c63b8176e7058d3b0
Rohan_Islam <rohanislam3669@gmail.com>
2024-06-30 07:15:30 +0600

    first

---
+++ components/home-section/navbar.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="container">
+        <div>
+          <img src="/image.png" alt="" />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
