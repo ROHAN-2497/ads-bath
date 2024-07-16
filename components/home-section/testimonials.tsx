4016dd3d52d84294255017478e781af653f192a8
Rohan_Islam <rohanislam3669@gmail.com>
2024-07-17 05:47:23 +0600

    update

--- components/home-section/testimonials.tsx
+++ components/home-section/testimonials.tsx
@@ -56,16 +56,26 @@ const Testimonials = () => {
           <img className="w-[443px] h-[300px]" src="/bgChose.png" alt="" />
         </div>
       </div>
-      <div>
-        <div className="flex flex-col">
+      <div className="flex mt-20 px-36 gap-20">
+        <div className="flex flex-col text-white gap-20 w-[35%]">
           <h3 className="text-3xl font-semibold">
             What Our Member Say About Us?
           </h3>
-          <img className="" src="/image copy 7.png" alt="" />
-          <img className="" src="/image copy 6.png" alt="" />
-          <img className="" src="/image copy 5.png" alt="" />
+          <div className="flex mx-20">
+            {" "}
+            <img
+              className="w-[56px] h-[56px]"
+              src="/image copy 10.png"
+              alt=""
+            />
+            <img className="w-[56px] h-[56px]" src="/image copy 9.png" alt="" />
+            <img className="w-[56px] h-[56px]" src="/image copy 8.png" alt="" />
+          </div>
+          <p> 10K+ Satisfied Customer</p>{" "}
         </div>
-        <div></div>
+        <div className="w-[65%]">
+          <img src="/image copy 11.png" alt="" />
+        </div>
       </div>
     </div>
   );
